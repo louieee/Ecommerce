9d366402522dbb65d6957df5628defc07e9f08bb
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2024-10-18 17:22:04 +0100

    update

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -160,6 +159,7 @@ REST_FRAMEWORK = {
     "DATE_FORMAT": "%Y-%m-%d",
     "TIME_FORMAT": "%H:%M:%S"
 }
+STATIC_URL = '/static/'
 STATIC_ROOT = "static"
-STATIC_DIR = (os.path.join(BASE_DIR, "static"),)
+STATIC_DIR = (os.path.join(BASE_DIR.parent, "static"),)
 
